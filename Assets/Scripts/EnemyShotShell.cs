02dc8e75d8cd3d8f23feeab11bb534d5b9fb02bf
snowman20TI048 <snowman2000y@gmail.com>
2022-10-19 01:30:15 +0900

    7章まで

---
+++ Assets/Scripts/EnemyShotShell.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotShell : MonoBehaviour
+{
+    public float shotSpeed;
+    [SerializeField]
+    private GameObject enemyShellPrefab;
+    [SerializeField]
+    private AudioClip shotSound;
+    private int interval;
+
+    void Update()
+    {
+        interval += 1;
+
+        if (interval % 60 == 0)
+        {
+            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
+
+            Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();
+
+            // forwardはZ軸方向（青軸方向）・・・＞この方向に力を加える。
+            enemyShellRb.AddForce(transform.forward * shotSpeed);
+
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+
+            Destroy(enemyShell, 3.0f);
+        }
+    }
+}
